93ecfd3c7b1f238f4544e7d0a9f7fa4586f47383
SShSoftwareEngineer <shypulin@ukr.net>
2025-02-01 00:13:23 +0200

    The structure of the project has been developed

---
+++ database_handler.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+
+def create_db():
+    pass
+
+
+def init_db():
+    pass
+
+
+def get_db_connection():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ telegram_handler.py
@@ -0,0 +1,5 @@
+from telethon import TelegramClient
+from telethon.sessions import MemorySession
+
+if __name__ == '__main__':
+    pass
